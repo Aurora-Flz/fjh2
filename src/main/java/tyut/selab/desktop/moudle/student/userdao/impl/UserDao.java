612d1506e135806b42579eb0d8243d316d2ad6fa
flz <2522205668@qq.com>
2023-04-12 18:50:39 +0800

    数据库完成

--- src/main/java/tyut/selab/desktop/moudle/student/userdao/impl/UserDao.java
+++ src/main/java/tyut/selab/desktop/moudle/student/userdao/impl/UserDao.java
@@ -1,6 +1,7 @@
 package tyut.selab.desktop.moudle.student.userdao.impl;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import tyut.selab.desktop.moudle.student.domain.Role;
 import tyut.selab.desktop.moudle.student.domain.User;
@@ -18,7 +19,7 @@ public class UserDao implements IUserDao {
     public List<User> queryUser() {
         Connection connection =null;
         try {
-            String sql = "select * from actor";
+            String sql = "select * from user";
             connection = MysqlConnect.getConnection();
             return qr.query(connection,sql,new BeanListHandler<>(User.class));
         } catch (SQLException e) {
@@ -27,49 +28,129 @@ public class UserDao implements IUserDao {
             MysqlConnect.close(null,null,connection);
         }
     }
-
     @Override
     public User queryUserByStudentNumber(Integer studentNumber) {
-        return null;
+        Connection connection = null;
+        try {
+            connection  = MysqlConnect.getConnection();
+            String sql  ="select * from user where student_number = ?";
+            return qr.query(connection,sql,new BeanHandler<User>(User.class),studentNumber);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-
     @Override
     public User queryUserByStudentName(String name) {
-        return null;
+        Connection connection = null;
+        try {
+            connection  = MysqlConnect.getConnection();
+            String sql  ="select * from user where name = ?";
+            return qr.query(connection,sql,new BeanHandler<User>(User.class),name);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-
     @Override
     public int insertUser(User user) {
-        return 0;
+        Connection connection =null;
+        try {
+            connection = MysqlConnect.getConnection();
+            String sql = "insert into user value(null,?,?,?,?,?,?,?,?,?,?)";
+            int insert = qr.update(connection,sql,user.getAccountNumber(),
+                    user.getPassword(),user.getName(),user.getStudentNumber(),
+                    user.getGender(),user.getPhone(),
+                    user.getPost(),user.getRegisterTime(),
+                    user.getLoginStatus(),user.getRoleId());
+            return (insert > 0? 1 : -1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
     }
-
     @Override
     public int updateUser(String password, User oldUser) {
-        return 0;
+        Connection connection =null;
+        try {
+            connection = MysqlConnect.getConnection();
+            String sql = "update user set password =? where user_id =?";
+            int update = qr.update(connection,sql,password,oldUser);
+            return (update > 0? 1 : -1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
     }
-
     @Override
     public int deleteUser(int studentNumber) {
-        return 0;
+        Connection connection =null;
+        try {
+            connection = MysqlConnect.getConnection();
+            String sql = "delete from user where student_number = ?";
+            int delete = qr.update(connection,sql,studentNumber);
+            return (delete > 0? 1 : -1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
     }
-
     @Override
     public List<Role> queryAllRole() {
-        return null;
+        Connection connection =null;
+        try {
+            connection = MysqlConnect.getConnection();
+            String sql = "select * from user_role";
+            return qr.query(connection,sql,new BeanListHandler<Role>(Role.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
     }
-
     @Override
     public int insertRole(Role role) {
-        return 0;
+        Connection connection =null;
+        try {
+            connection = MysqlConnect.getConnection();
+            String sql = "insert into user_role value(null,?)";
+            int insert = qr.update(connection,sql,role.getDuty());
+            return (insert > 0? 1 : -1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
     }
 
     @Override
     public int updateRole(Role newRole, Role oleRole) {
-        return 0;
+        Connection connection =null;
+        try {
+            connection = MysqlConnect.getConnection();
+            String sql = "update user_role set duty =? where duty = ?";
+            int update = qr.update(connection,sql,newRole,oleRole);
+            return (update > 0? 1 : -1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
     }
 
     @Override
     public int deleteRole(Role role) {
-        return 0;
+        Connection connection =null;
+        try {
+            connection = MysqlConnect.getConnection();
+            String sql = "delete from user_role where duty = ?";
+            int update = qr.update(connection,sql,role);
+            return (update > 0? 1 : -1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
     }
 }
